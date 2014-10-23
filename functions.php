6828e21cc0ecedf1a4080773b84c8a9dcaf465b9
Niklas P <contact@niklasplessing.net>
2014-10-23 02:17:33 +0200

    bxslider controls

--- functions.php
+++ functions.php
@@ -248,7 +248,6 @@ function parse_gallery_shortcode($atts) {
 			$output .= '<li><img src="' . $large_url[0] .'" /></li>';
 		}
 		$output .= '</ul>';
-		$output .= '<div class="bx-controls bx-has-controls-direction"><div class="bx-controls-direction"><a class="bx-prev" href="">Prev</a><a class="bx-next" href="">Next</a></div></div></div>';
 		// $output .= '<div id="bx-pager">';
 		// foreach($images as $idx => $image) {
 		// 	$thumb_url = wp_get_attachment_image_src( $image->ID, 'thumbnail');
