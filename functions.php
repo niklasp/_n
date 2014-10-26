674b4b401fe2f73189e9b8f4dbf50e58ae378e5b
Niklas P <contact@niklasplessing.net>
2014-10-26 03:27:18 +0100

    settings page + google analyticks tracking setting

--- functions.php
+++ functions.php
@@ -315,16 +315,17 @@ add_action('print_media_templates', function(){
 function _n_analytics_code() {
 	?>
 
-	<script>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-	  ga('create', <?php echo esc_attr(get_option("_n_ga_id")); ?>, 'auto');
-	  ga('send', 'pageview');
-
+	<script type="text/javascript">
+		var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+		document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+		</script>
+	<script type="text/javascript">
+		try{
+			var pageTracker = _gat._getTracker("<?php echo esc_attr(get_option("_n_ga_id")); ?>");
+			pageTracker._trackPageview();
+		} catch(err) {}
 	</script>
+
 	<?php
 
 }
